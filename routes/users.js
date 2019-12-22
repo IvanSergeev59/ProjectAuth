efbccd4aff55642d6532d76aa2cce6f4bcd1e5ad
Ivan Sergeev <ahrejlok2007@ya.ru>
2019-12-22 15:56:49 +0500

    9 commit

--- routes/users.js
+++
@@ -1,6 +0,0 @@
-const routerUser = require('express').Router();
-const { getUsers, getUsersId } = require('../controllers/users');
-
-routerUser.get('/', getUsers);
-routerUser.get('/:userId', getUsersId);
-module.exports = routerUser;
